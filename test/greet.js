1dad9558a111b86ce6250b5f4b2dc9a70c0fb36c
Asavelawayiza <20asavelawayiza@gmail.com>
2019-07-17 10:30:25 +0200

    first commit

---
+++ test/greet.js
@@ -0,0 +1,22 @@
+function greet(name){
+  var name = '';
+    return('Hello' + ', ' + name);
+
+  };
+
+  var greeting = greet(name);
+  
+ 
+    function Language() {
+        if (Language = english)
+        return "Hello";
+    }
+        if(Language = Xhosa){
+            return "Molo";
+        }
+
+        var greeting = greet(name);
+
+
+
+  
---
+++ test/greet.test.js
@@ -0,0 +1,6 @@
+describe('greet test' , function(){
+    it('takes name as a parameter and returns a greeting message ' , function(){
+        assert.equal(greet(""), greeting);
+
+    })
+});
